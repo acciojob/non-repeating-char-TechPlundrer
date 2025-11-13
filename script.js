48879c86e132bd175b8b06bf76ba6f74819ed292
Vishal Patil <vishalpatilvgp2199@gmail.com>
2025-11-13 16:07:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function firstNonRepeatedChar(str) 
+function firstNonRepeatedChar(str) {
  // Write your code here
 if(!str || str.length === 0) return null;
 
@@ -7,13 +7,14 @@ for(let i = 0; i < str.length; i++){
 	const c = str[i];
 	freq[c] = (freq[c] || 0) + 1; 
 }
+
 	for(let i = 0; i < str.length; i++){
 		const c = str[i];
 		if(freq[c] === 1) {
 			return c;
 		}
 	}
-return null;
+return null; 
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));  
